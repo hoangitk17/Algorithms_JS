fd09d160a6b6f7aa71282237f686ffabc62507f3
Nguyen Van Hoang <hoangitk17@gmail.com>
2022-03-24 23:24:48 +0700

    Update LinkedListNode class

--- src/data-structures/linked-list/LinkedListNode.js
+++ src/data-structures/linked-list/LinkedListNode.js
@@ -1,10 +1,30 @@
 export default class LinkedListNode {
+  #value = undefined;
+  #next = null;
+
   constructor(value, next = null) {
-    this.value = value;
-    this.next = next;
+    this.#value = value;
+    this.#next = next;
+  }
+
+  get value() {
+    return this.#value;
+  }
+
+  set value(value) {
+    this.#value = value;
+  }
+
+  get next() {
+    return this.#next;
+  }
+
+  set next(next) {
+    this.#next = next;
   }
 
   toString(callback) {
-    return callback ? callback(this.value) : `${this.value}`;
+    return callback ? callback(this.#value) : `${this.#value}`;
   }
+
 }
