deaa9a4e9317a2b1bb62b5096b672e220790ef6f
Nguyen Van Hoang <hoangitk17@gmail.com>
2022-03-28 23:19:09 +0700

    Add test for Comparator class

---
+++ src/utils/test/Comparator.test.js
@@ -0,0 +1,63 @@
+import Comparator from "../Comparator";
+
+describe("Comparator", () => {
+  it("should not equal with two number", () => {
+    const comparator = new Comparator();
+    expect(comparator.equal(1, 2)).toBe(false);
+  });
+
+  it("should not equal with two number", () => {
+    const comparator = new Comparator();
+    expect(comparator.equal(2, 1)).toBe(false);
+  });
+
+  it("should equal with two number", () => {
+    const comparator = new Comparator();
+    expect(comparator.equal(1, 1)).toBe(true);
+  });
+
+  it("should a less than b", () => {
+    const comparator = new Comparator();
+    expect(comparator.lessThan(1, 2)).toBe(true);
+  });
+
+  it("should a not less than b", () => {
+    const comparator = new Comparator();
+    expect(comparator.lessThan(2, 1)).toBe(false);
+  });
+
+  it("should a not less than b", () => {
+    const comparator = new Comparator();
+    expect(comparator.lessThan(1, 1)).toBe(false);
+  });
+
+  it("should a greater than b", () => {
+    const comparator = new Comparator();
+    expect(comparator.greaterThan(1, 0)).toBe(true);
+  });
+
+  it("should a not greater than b", () => {
+    const comparator = new Comparator();
+    expect(comparator.greaterThan(1, 1)).toBe(false);
+  });
+
+  it("should a not greater than b", () => {
+    const comparator = new Comparator();
+    expect(comparator.greaterThan(0, 2)).toBe(false);
+  });
+
+  it("should a less than or equal b ", () => {
+    const comparator = new Comparator();
+    expect(comparator.lessThanOrEqual(1, 2)).toBe(true);
+  });
+
+  it("should a less than or equal b", () => {
+    const comparator = new Comparator();
+    expect(comparator.lessThanOrEqual(1, 1)).toBe(true);
+  });
+
+  it("should a not less than or equal b", () => {
+    const comparator = new Comparator();
+    expect(comparator.lessThanOrEqual(2, 1)).toBe(false);
+  });
+});
