1beba8ae53e6cff6488bef17da52a982cdde37b6
Nguyen Van Hoang <hoangitk17@gmail.com>
2022-04-01 23:13:25 +0700

    Update test DoubleLinkedNode

--- src/data-structures/doubly-linked-list/test/DoublyLinkedListNode.test.js
+++ src/data-structures/doubly-linked-list/test/DoublyLinkedListNode.test.js
@@ -4,5 +4,15 @@ describe('DoublyLinkedListNode', () => {
   it("Init double linked list node", () => {
     let node = new DoublyLinkedListNode();
     expect(node.value).toBeUndefined();
+    expect(node.previous).toBeNull();
+    expect(node.next).toBeNull();
+  })
+
+  it("Init double linked list node 2", () => {
+    let node = new DoublyLinkedListNode(5);
+    expect(node.value).toBe(5);
+    expect(node.previous).toBeNull();
+    expect(node.next).toBeNull();
+    expect(node.toString()).toBe("5");
   })
 });
