94e92906a5a31d89f10b55176165b96d0b5941d6
Nguyen Van Hoang <hoangitk17@gmail.com>
2022-04-02 23:32:47 +0700

    Update test next and previous of node

--- src/data-structures/doubly-linked-list/test/DoublyLinkedListNode.test.js
+++ src/data-structures/doubly-linked-list/test/DoublyLinkedListNode.test.js
@@ -15,4 +15,15 @@ describe('DoublyLinkedListNode', () => {
     expect(node.next).toBeNull();
     expect(node.toString()).toBe("5");
   })
+
+  it("Init double linked list node with next and previous", () => {
+    let node1 = new DoublyLinkedListNode(6);
+    let node2 = new DoublyLinkedListNode(7);
+    node1.next = node2;
+    node2.previous = node1;
+    expect(node1.previous).toBeNull();
+    expect(node1.next).toBe(node2);
+    expect(node2.previous).toBe(node1);
+    expect(node2.next).toBeNull();
+  })
 });
