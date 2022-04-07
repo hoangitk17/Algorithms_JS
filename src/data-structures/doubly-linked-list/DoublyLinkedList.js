8b3cef26e7600433fe1904c571bf9842d874b69c
Nguyen Van Hoang <hoangitk17@gmail.com>
2022-04-07 23:07:52 +0700

    Add DoublyLinkedList class

---
+++ src/data-structures/doubly-linked-list/DoublyLinkedList.js
@@ -0,0 +1,59 @@
+import DoublyLinkedListNode from "./DoublyLinkedListNode";
+import Comparator from "../../utils/Comparator";
+
+export default class DoublyLinkedList {
+  /**
+   * @param {Function} [comparatorFunction]
+   */
+  constructor(comparatorFunction) {
+    /** @var DoublyLinkedListNode */
+    this.head = null;
+    /** @var DoublyLinkedListNode */
+    this.tail = null;
+    this.compare = new Comparator(comparatorFunction);
+  }
+
+  /**
+   * @param {*} value
+   * @return {DoublyLinkedList}
+   */
+  prepend(value) {
+    // Biến nút mới thành head.
+    const newNode = new DoublyLinkedListNode(value, this.head);
+
+    // Nếu đã có head, thì nút này không còn là head nữa.
+    // Do dó, tham chiếu previous của nó sẽ hướng đến nút mới (head mới).
+    // Đồng thời nút mới sẽ là head.
+    if (this.head) {
+      this.head.previous = newNode;
+    }
+    this.head = newNode;
+    // Nếu vẫn chưa có tail thì nó sẽ là tail.
+    if (this.tail === null) {
+      this.tail = newNode;
+    }
+    return this;
+  }
+
+  /**
+   * @param {*} value
+   * @return {DoublyLinkedList}
+   */
+  append(value) {
+    const newNode = new DoublyLinkedListNode(value);
+
+    // Nếu vẫn chưa có head thì tạo nút mới thành head, tương tự với tail.
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
+    }
+    // Gắn tham chiếu next của tail thành nút mới.
+    this.tail.next = newNode;
+    // Gán tham chiếu previous của nút mới vào tail
+    newNode.previous = this.tail;
+    // Nút mới trở thành tail trong danh sách liên kết
+    this.tail = newNode;
+    return this;
+  }
+}
