ae28fddd6865207c197210e2cc01953dddf969f7
Nguyen Van Hoang <hoangitk17@gmail.com>
2022-04-16 23:01:16 +0700

    Add test delete head

--- src/data-structures/doubly-linked-list/test/DoublyLinkedList.test.js
+++ src/data-structures/doubly-linked-list/test/DoublyLinkedList.test.js
@@ -17,4 +17,17 @@ describe("DoublelyLinkedList", () => {
     list.fromArray([1, 2, 3]);
     expect(list.toString()).toBe("1,2,3");
   });
+
+  it("Delete head", () => {
+    const list = new DoublyLinkedList();
+    list.append(5);
+    list.append(6);
+    list.append(7);
+    expect(list.toString()).toBe("5,6,7");
+    list.deleteHead();
+    expect(list.toString()).toBe("6,7");
+    expect(list.deleteHead().value).toBe(6);
+    expect(list.deleteHead().value).toBe(7);
+    expect(list.deleteHead()).toBeNull();
+  });
 });
