0866691957c84534da395a47b18b4ddae55d6fa8
Nguyen Van Hoang <hoangitk17@gmail.com>
2022-04-24 23:17:50 +0700

    Update function in queue2 class

--- src/data-structures/queue/Queue2.js
+++ src/data-structures/queue/Queue2.js
@@ -1,39 +1,65 @@
 export default class Queue2 {
-  constructor() {
-    // Chúng ta sẽ triển khai Hàng đợi dựa trên danh sách liên kết vì
-    // cấu trúc của chúng khá tương đồng. Cụ thể, chúng đều hoạt động dựa trên
-    // phần tử đầu và cuối. So sánh thao tác enqueue/dequeue của hàng đợi
-    // với append/deleteHead của danh sách liên kết.
+  constructor(...args) {
+    this.elements = [...args];
+  }
+
+  get length() {
+    this.elements.length;
   }
 
   /**
    * @return {boolean}
    */
-  isEmpty() {}
+  get isEmpty() {
+    return this.length === 0;
+  }
 
   /**
    * Đọc phần tử đầu tiên của hàng đợi mà không xoá nó.
    * @return {*}
    */
-  peek() {}
+  peek() {
+    return this.elements[0];
+  }
 
   /**
    * Thêm một phần tử mới vào hàng đợi (tail của danh sách liên kết)
    * Phần tử đó sẽ được xử lý sau tất cả các phần tử ở trước nó.
    * @param {*} value
    */
-  enqueue(value) {}
+  enqueue(value) {
+    this.elements.push(value);
+  }
 
   /**
    * Xoá phần tử ở vị trí đầu tiên khỏi hàng đợi (head ở danh sách liên kết).
    * Nếu hàng đợi trống trả về null.
    * @return {*}
    */
-  dequeue() {}
+  dequeue() {
+    return this.elements.shift();
+  }
 
   /**
    * @param [callback]
    * @return {string}
    */
-  toString(callback) {}
+  toString(callback) {
+    return callback ? callback(this.elements) : this.elements.toString();
+  }
+
+  static fromArray(arr) {
+    return new Queue2(...arr);
+  }
+
+  toArray() {
+    return this.elements;
+  }
+
+  /**
+   * Xóa hết các phần tử trong queue
+   */
+  clear() {
+    this.elements = [];
+  }
 }
