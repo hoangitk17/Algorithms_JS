79f54fcc02a86021aa2463dfa73d377bbac9f4eb
Nguyen Van Hoang <hoangitk17@gmail.com>
2022-05-01 22:30:25 +0700

    Update stack2

--- src/data-structures/stack/Stack2.js
+++ src/data-structures/stack/Stack2.js
@@ -1,40 +1,74 @@
 export default class Stack2 {
-  constructor() {
-    this.elements = [];
+  static UNLIMITED_CAPACITY = -1;
+  #elements = [];
+  #capacity = null;
+
+  /**
+   * @param {number} capacity
+   */
+  constructor(capacity = Stack2.UNLIMITED_CAPACITY) {
+    this.setCapacity(capacity);
+  }
+
+  /**
+   * @param {*} value
+   * @returns {boolean}
+   */
+  #isInt(value) {
+    return !isNaN(value) && parseInt(value) == value;
+  }
+
+  /**
+   * @param {number} capacity
+   */
+  setCapacity(capacity) {
+    if (this.#isInt(capacity)) throw Error("Capacity is not a number");
+    if (capacity < 0 && capacity !== Stack2.UNLIMITED_CAPACITY)
+      throw Error("Capacity is not negative number");
+    this.#capacity = capacity;
   }
 
   /**
    * @return {boolean}
    */
-  isEmpty() {
+  get isEmpty() {
     return this.size === 0;
   }
 
   /**
-   * @return {*}
+   * @return {boolean}
    */
-  peek() {
-    return this.elements[this.size - 1];
+  get isFull() {
+    return this.size === this.#capacity;
   }
 
   /**
    * @return {number}
    */
   get size() {
-    return this.elements.length;
+    return this.#elements.length;
+  }
+
+  /**
+   * @return {*}
+   */
+  peek() {
+    if (this.isEmpty) return undefined;
+    return this.#elements[this.size - 1];
   }
 
   /**
    * @param {*} value
    */
   push(value) {
-    return this.elements.push(value);
+    if (this.isFull) return undefined;
+    return this.#elements.push(value);
   }
 
   /**
    * @return {*}
    */
   pop() {
-    return this.elements.pop();
+    return this.#elements.pop();
   }
 }
